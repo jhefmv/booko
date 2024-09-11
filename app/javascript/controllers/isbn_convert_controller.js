ca4ab70f741e405d052dc4949af81993c620ce17
Jhef Vicedo <jhefmv@gmail.com>
2024-09-11 08:58:01 +0800

    Adds validation to convert form

--- app/javascript/controllers/isbn_convert_controller.js
+++ app/javascript/controllers/isbn_convert_controller.js
@@ -6,11 +6,26 @@ import { isbnConvert } from "custom_modules/isbn/convert"
 export default class extends Controller {
   static targets = [ "fromIsbn", "toIsbn" ]
 
+  invalidateFormInput(msg) {
+    const isbnErrorEl = document.getElementById('validationIsbnFeedback')
+    isbnErrorEl.innerText = msg
+    this.fromIsbnTarget.classList.add("is-invalid")
+  }
+
   convert(e) {
     e.preventDefault()
 
-    if (isbnValid(this.fromIsbnTarget.value)) {
-      this.toIsbnTarget.value = isbnConvert(this.fromIsbnTarget.value)
+    const fromIsbn = this.fromIsbnTarget.value
+
+    if (!fromIsbn) {
+      this.invalidateFormInput("ISBN is required")
+    } else {
+      if (isbnValid(this.fromIsbnTarget.value)) {
+        this.toIsbnTarget.value = isbnConvert(this.fromIsbnTarget.value)
+        this.toIsbnTarget.classList.add("is-valid")
+      } else {
+        this.invalidateFormInput("ISBN is invalid")
+      }
     }
   }
 }
